bc94dcf4f138218b41997597c5c4906dea31f70f
Awwab <mawwabkhank2006@gmail.com>
2024-02-04 21:29:18 +0500

    paddle up and down movement

--- main.py
+++ main.py
@@ -12,4 +12,16 @@ paddle.shapesize(stretch_wid=5, stretch_len=1)
 paddle.penup()
 paddle.goto(350, 0)
 
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
+
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
+
+screen.listen()
+screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
+
 screen.exitonclick()
