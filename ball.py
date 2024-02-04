deb9c65537b1b208724f19d846e2071b368491fe
Awwab <mawwabkhank2006@gmail.com>
2024-02-04 21:59:58 +0500

    created ball class to handle ball state and behaviour

---
+++ ball.py
@@ -0,0 +1,12 @@
+from turtle import Turtle
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("white")
+        self.penup()
+        self.move()
+
+    def move(self):
+        self.goto(self.xcor()+10, self.ycor()+10)
