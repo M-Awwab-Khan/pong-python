7cd6b28648c2cb9563541aed059c919a39653fbb
Awwab <mawwabkhank2006@gmail.com>
2024-02-04 22:00:11 +0500

    import ball and made instance

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.bgcolor("black")
@@ -9,6 +11,7 @@ screen.tracer(0)
 
 r_paddle = Paddle((-350, 0))
 l_paddle = Paddle((350, 0))
+ball = Ball()
 
 screen.listen()
 screen.onkey(l_paddle.go_up, "w")
@@ -19,6 +22,8 @@ screen.onkey(r_paddle.go_down, "Down")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
 
 screen.exitonclick()
